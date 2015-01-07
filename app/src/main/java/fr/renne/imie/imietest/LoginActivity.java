0286fc19d1db5dac7801d862a1aed7dc76458b8c
Pol Natral <Pol.Natral@gmail.com>
2015-01-07 16:09:55 +0100

    Imie Projet

---
+++ app/src/main/java/fr/renne/imie/imietest/LoginActivity.java
@@ -0,0 +1,17 @@
+package fr.renne.imie.imietest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+/**
+ * Created by Pol on 07/01/2015.
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_login);
+    }
+}
